d7ef099ba9a217aa58d8f214566c14107941d470
Googlepony <ponybitly@gmail.com>
2025-01-07 18:38:25 +0530

    check

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteStaticCopy()],
+  plugins: [react()],
   base: '/tvfwebsite/',
   optimizeDeps: {
     exclude: ['lucide-react'],
